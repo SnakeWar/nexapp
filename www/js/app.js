65a445278fb80ec068b462c525223ac0c9446404
Mayrcon <mayrcon_marlon@hotmail.com>
2017-03-14 13:17:38 -0300

    Páginas terminadas: Home, quem somos, serviços, clientes e localização

---
+++ www/js/app.js
@@ -0,0 +1,83 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            setTimeout(function() {
+             navigator.splashscreen.hide();
+             }, 300);
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/home.html'
+            })
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html'
+            })
+            .state('app.solicite', {
+                url: '/solicite',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/solicite.html'
+                    }
+                }
+            })
+            .state('app.services', {
+                url: '/services',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/services.html'
+                    }
+                }
+            })
+            .state('app.quemsomos', {
+                url: '/quemsomos',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/quemsomos.html'
+                    }
+                }
+            })
+            .state('app.clientes', {
+                url: '/clientes',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/clientes.html'
+                    }
+                }
+            })
+            .state('app.localizacao', {
+                url: '/localizacao',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/localizacao.html',
+                        controller: 'MapCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/clientes');
+    });
+
---
+++ www/js/controllers.js
@@ -0,0 +1,95 @@
+angular.module('starter.controllers', [])
+    .controller('MapCtrl', function ($scope) {
+        $scope.map = {};
+        $scope.latLng1 = null;
+
+        var mapOptions1 = {
+            center: new google.maps.LatLng(-5.5845379, -36.730328),
+            zoom: 7,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        };
+
+        var icon = {
+            url: "img/pin-map.png",
+            scaledSize: new google.maps.Size(40, 40)/*,
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(0, 0)*/
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions1);
+
+        /* Marcador Natal*/
+        var natal = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(-5.8174936, -35.2334466),
+            icon: icon
+        });
+        var infoWindow1 = new google.maps.InfoWindow({
+            content: "<b>NATAL (MATRIZ)</b>" +
+            "<br>RUA DOS CAICOS, 2305 - LOJA D, NSRA. NAZARÉ" +
+            "<br>(84) 2030-4080"
+        });
+        google.maps.event.addListener(natal, 'click', function () {
+            infoWindow1.open($scope.map, natal);
+        });
+        /* Marcador João Pessoa*/
+        var jpessoa = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(-7.1421556, -34.8853253),
+            icon: icon
+    });
+        var infoWindow2 = new google.maps.InfoWindow({
+            content: "<b>JOÃO PESSOA</b>"+
+            "<br>RUA ALCIDES BEZERRA, 693 CRUZ DAS ARMAS"+
+            "<br>(83) 3242-0227"+
+            "<br>(84) 99104-9940"
+        });
+        google.maps.event.addListener(jpessoa, 'click', function () {
+            infoWindow2.open($scope.map, jpessoa);
+        });
+        /* Marcador Campina Grande*/
+        var cgrande = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(-7.2415412, -35.893462),
+            icon: icon
+    });
+        var infoWindow3 = new google.maps.InfoWindow({
+            content: "<b>CAMPINA GRANDE</b>"+
+            "<br>RUA PAPA JOÃO XXIII, 355 LIBERDADE"+
+            "<br>(83) 3331-4411"+
+            "<br>(84) 99106-2735"
+        });
+        google.maps.event.addListener(cgrande, 'click', function () {
+            infoWindow3.open($scope.map, cgrande);
+        });
+        /* Marcador Fortaleza*/
+        var fortaleza = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(-3.7624156, -38.5419236),
+            icon: icon
+    });
+        var infoWindow4 = new google.maps.InfoWindow({
+            content: "<b>FORTALEZA</b>"+
+            "<br>RUA ARMANDO MONTEIRO, 333 VILA UNIÃO"+
+            "<br>(85) 3256-3299"+
+            "<br>(84) 99104-6558"
+        });
+        google.maps.event.addListener(fortaleza, 'click', function () {
+            infoWindow4.open($scope.map, fortaleza);
+        });
+        /* Marcador Mossoró*/
+        var mossoro = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(-5.1845379, -37.350328),
+            icon: icon
+    });
+        var infoWindow5 = new google.maps.InfoWindow({
+            content: "<b>MOSSORÓ</b>"+
+            "<br>RUA JOÃO LUIZ G DE MELO, SN AEROPORTO"+
+            "<br>(84) 99104-7310"
+        });
+        google.maps.event.addListener(mossoro, 'click', function () {
+            infoWindow5.open($scope.map, mossoro);
+        });
+    });
